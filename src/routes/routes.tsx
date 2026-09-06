c8e40ae21a1b634e38d46056c67a18fbc68ce5f0
Carlos <ChsSilva1419@gmail.com>
2026-09-06 18:54:50 -0300

    feat: criação da tela detalhes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { EmConstrucao } from './emConstrucao';
 import TesteComponentes from '../pages/TesteComponentes';
+import { DetalhesChamado } from '../pages/Detalhes';
 // import { HomeLayout } from '../components/layouts/HomeLayout';
 // import { FormLayout } from '../components/layouts/FormLayout';
 
@@ -53,6 +54,10 @@ export const router = createBrowserRouter([
         element: <EmConstrucao nome="Revisão" />,
       },
     ],
+  },
+  {
+    path: '/detalhes',
+    element: <DetalhesChamado />,
   },
   {
     path: '*',
