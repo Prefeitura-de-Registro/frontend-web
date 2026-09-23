d120fd99792cf4153345c4522e4d0c264fc86657
Art0_alb <arthuralbinot844@gmail.com>
2026-09-23 18:11:36 -0300

    feat: mockando os dados na page SignIn (em andamento)

--- src/mock/credencial.mock.ts
+++ src/mock/credencial.mock.ts
@@ -1,46 +1,16 @@
 import type { Credenciais } from '../types/credenciais';
 
 export const credencialMock: Credenciais[] = [
-  {
-    matricula: 'ebonybeck@limozen.com',
-    senha: 'ace7160a-0',
-  },
-  {
-    matricula: 'christiebeck@limozen.com',
-    senha: '1ed38aa3-3',
-  },
-  {
-    matricula: 'hydebeck@limozen.com',
-    senha: '7676ce4c-6',
-  },
-  {
-    matricula: 'marisolbeck@limozen.com',
-    senha: '50c49ec0-b',
-  },
-  {
-    matricula: 'bishopbeck@limozen.com',
-    senha: 'e3afd84c-3',
-  },
-  {
-    matricula: 'baxterbeck@limozen.com',
-    senha: '976b8328-1',
-  },
-  {
-    matricula: 'lorenebeck@limozen.com',
-    senha: '7d98380d-2',
-  },
-  {
-    matricula: 'franksbeck@limozen.com',
-    senha: 'b143d512-6',
-  },
-  {
-    matricula: 'vondabeck@limozen.com',
-    senha: 'c724f316-f',
-  },
-  {
-    matricula: 'melendezbeck@limozen.com',
-    senha: 'c3fbef50-f',
-  },
+  { email: 'sabrinaps@gmail.com', senha: 'ace7160a-0' },
+  { email: 'christiebeck@gmail.com', senha: '1ed38aa3-3' },
+  { email: 'hydebeck@gmail.com', senha: '7676ce4c-6' },
+  { email: 'marisolbeck@gmail.com', senha: '50c49ec0-b' },
+  { email: 'bishopbeck@gmail.com', senha: 'e3afd84c-3' },
+  { email: 'baxterbeck@gmail.com', senha: '976b8328-1' },
+  { email: 'lorenebeck@gmail.com', senha: '7d98380d-2' },
+  { email: 'franksbeck@gmail.com', senha: 'b143d512-6' },
+  { email: 'vondabeck@gmail.com', senha: 'c724f316-f' },
+  { email: 'melendezbeck@gmail.com', senha: 'c3fbef50-f' },
 ];
 
 export default credencialMock;
--- src/types/credenciais.ts
+++ src/types/credenciais.ts
@@ -1,5 +1,5 @@
 interface Credencial {
-  matricula: string;
+  email: string;
   senha: string;
 }
 
